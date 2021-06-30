d57d80a83ca00220da16c1146585876ce6e757f6
luis banda <rockband_666@hotmail.com>
2021-06-29 22:38:18 -0400

    creando la funcion para saber con el precio y descuento

--- descuentos.js
+++ descuentos.js
@@ -2,13 +2,21 @@ const precioOriginal = 100;
 const descuento = 15;
 
 
-const porcentajeDelPrecioConDescuento = 100 - descuento;
+// const porcentajeDelPrecioConDescuento = 100 - descuento;
 
-const precioConDescuento = (precioOriginal * porcentajeDelPrecioConDescuento)/ 100;
+// const precioConDescuento = (precioOriginal * porcentajeDelPrecioConDescuento)/ 100;
 
-console.log({
-    descuento,
-    porcentajeDelPrecioConDescuento,
-    precioConDescuento,
-    precioOriginal,
-});
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajeDelPrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajeDelPrecioConDescuento)/ 100;
+
+    return precioConDescuento;
+
+}
+
+// console.log({
+//     descuento,
+//     porcentajeDelPrecioConDescuento,
+//     precioConDescuento,
+//     precioOriginal,
+// });
